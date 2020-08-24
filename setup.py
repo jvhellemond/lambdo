e351b5bfff3fb820d8732276de6c6b88ee45393e
Jan van Hellemond <jan@frontlab.nl>
2020-08-24 21:54:34 +0200

    More meta.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-# @todo: Add lots of package meta information.
+# @todo: Add more package meta information for PyPI.
 setup(
 	name="lambdo",
 	description="Just lambdo it",
-	version="5.6",
+	long_description="@todo: Write a long description, format it as reStructuredText",
+	author="Jan van Hellemond",
+	author_email="jvhellemond@gmail.com",
+	url="https://github.com/jvhellemond/lambdo",
+	version="5.7",
 	install_requires=["boto3", "glob2", "PyYAML"],
 	py_modules=["lambdo"],
 	entry_points={"console_scripts": ["lambdo=lambdo:main"]}
